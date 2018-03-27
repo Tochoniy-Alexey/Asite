93db41d43b0e8aceb160f00b87f1628f4f49f700
Tochoniy Alexey <tochoniy_alexey@ukr.net>
2018-03-27 12:05:58 +0300

    Sait

---
+++ js/common.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+
+	
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	
+
+	//animateNumber
+	$(".numbers").waypoint(function() {
+		
+		$({blurRadius: 5}).animate({blurRadius: 0}, {
+			duration: 1200,
+			easing: 'swing',
+			step: function() {
+				$(".number-item h3 span").css({
+					"-webkit-filter": "blur("+this.blurRadius+"px)",
+					"filter": "blur("+this.blurRadius+"px)"
+				});
+			}
+		});
+		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+		$(".number-item h3 span").each(function() {
+			var tcount = $(this).data("count");
+			$(this).animateNumber({ number: tcount,
+				easing: 'easeInQuad',
+				"font-size": "1.8125em",
+				numberStep: comma_separator_number_step},
+				1200);
+		});
+		this.destroy();
+
+	}, {
+		offset: '90%'
+	});
+
+	//Magnific-Popup
+	$(".portfolio-item").each(function(e) {
+
+		var th = $(this);
+
+		th.attr("href", "#portfolio-img-" + e)
+			.find(".portfolio-popup")
+				.attr("id", "portfolio-img-" + e);
+
+	});
+
+	$(".portfolio-item").magnificPopup();
+
+	
+
+	//Каруселька
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 3
+	});
+	owl.on(".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	
+
+	//Кнопка "Наверх"
+	$("body").on("click", ".top", function() {
+		$("html, body").animate({scrollTop: 0}, "slow");
+	});
+	$("body").append('<div class="top"><i class="fa fa-angle-double-up">');
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() > $(this).height()) {
+			$(".top").addClass("active");
+		} else {
+			$(".top").removeClass("active");
+		}
+	});
+	
+	
+	//Аякс отправка форм
+	$("#callback").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("#callback").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
